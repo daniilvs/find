368ca16509ef47948576baa8f6061dd2c94608ac
daniilvs <dango41rus@yandex.ru>
2019-04-19 03:42:56 +0400

    Рекурсия приведена в порядок, добавлены дополнительны тесты, все работает + небольшие изменения по структуре

--- src/test/test.kt
+++ src/test/test.kt
@@ -1,9 +1,11 @@
-package tests
+package test
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import com.xenomachina.argparser.ArgParser
 import find.FindFile
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 fun helperForTests(args: Array<String>) =
     ArgParser(args).parseInto(::FindFile).find()
@@ -16,12 +18,16 @@ class Test {
         assertTrue(helperForTests(arrayOf("-r", "-d", "input/test", "test.txt")))
         assertTrue(helperForTests(arrayOf("-r", "sub_default.txt")))
         assertThrows(java.lang.IllegalArgumentException::class.java) {
-            helperForTests(arrayOf("-d", "F:/test/sub", "test.txt")) }
+            helperForTests(arrayOf("-d", "F:/test/sub", "test.txt"))
+        }
         assertThrows(java.lang.IllegalArgumentException::class.java) {
-            helperForTests(arrayOf("-d", "input/test/sub", "test.txt")) }
+            helperForTests(arrayOf("-d", "input/test/sub", "test.txt"))
+        }
         assertThrows(com.xenomachina.argparser.UnrecognizedOptionException::class.java) {
-            helperForTests(arrayOf("-s", "input/test/sub_3", "test.txt")) }
+            helperForTests(arrayOf("-s", "input/test/sub_3", "test.txt"))
+        }
         assertThrows(com.xenomachina.argparser.MissingRequiredPositionalArgumentException::class.java) {
-            helperForTests(arrayOf("-d", "input/test/sub_2")) }
+            helperForTests(arrayOf("-d", "input/test/sub_2"))
+        }
     }
 }
